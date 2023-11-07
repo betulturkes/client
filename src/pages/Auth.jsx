b8e9746744ff54b6abab659a090614326471c1a8
Betul Turkes <bbetulturkess@gmail.com>
2023-11-07 18:35:03 +0300

    gereksiz dosyalar silindi, db açıldı, frontend...

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth = () => {
+    return (
+        <div className = 'w-full h-screen bg-gray-50 flex items-center justify '>
+
+        </div>
+    )
+}
+
+export default Auth
